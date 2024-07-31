da5e25814ac87813c4ac35814ef6ddcbde2d6db8
py1u.dev <py1u.dev@gmail.com>
2024-07-31 13:28:08 -0700

    update: component structure order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,8 @@ import {
   applyUpscaler,
   applyControlNet,
 } from '@/app/api.js';
+import FeatureSection from '@/components/ui/features';
+import StorySection from '@/components/ui/story';
 
 type ArtStyleOption = string | { label: string; options: string[] };
 type ArtStyles = Record<string, ArtStyleOption[]>;
@@ -305,7 +307,7 @@ export default function Home() {
       <BlurFade>
         <div></div>
         <div className="flex flex-col items-center py-10">
-          <div className="lg:w-2/3 bg-gradient-to-r from-blue-300 to-indigo-600 bg-clip-text text-transparent   lg:my-16">
+          <div className="lg:w-2/3 bg-gradient-to-r  from-violet-500 to-slate-800 bg-clip-text text-transparent   lg:my-16">
             <h1 className="text-6xl font mb-4 ">PHiLIP</h1>
             <h2 className="text-3xl text-gray-950 mb-4 ">
               {' '}
@@ -314,6 +316,12 @@ export default function Home() {
           </div>
           <div>
             <InfoSection />
+          </div>
+          <div>
+            <FeatureSection />
+          </div>
+          <div>
+            <StorySection />
           </div>
           <div>
             <TeamSection />
