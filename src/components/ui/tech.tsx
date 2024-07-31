53eb25a27d9db35c8ed728e8f9c591d9bce91a39
py1u.dev <py1u.dev@gmail.com>
2024-07-31 15:53:21 -0700

    create: techsection component and display to page.tsx

---
+++ src/components/ui/tech.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+const TechSection: React.FC = () => {
+  return <div className="flex justify-center p-4"></div>;
+};
+
+export default TechSection;
