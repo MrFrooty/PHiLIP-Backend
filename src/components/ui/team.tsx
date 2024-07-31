58c83f057d267b3448625feb0aa48e7fe5ea0d01
py1u.dev <py1u.dev@gmail.com>
2024-07-30 22:00:04 -0700

    update: team cards with associated array object details

---
+++ src/components/ui/team.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+const teamMembers = [
+  {
+    name: 'Michael Chen',
+    grade: 'UCR 2nd year Computer Science',
+    role: 'Backend, Model Engineer',
+    hobbies: 'gaming and reading novels',
+  },
+  {
+    name: 'Freddy Song',
+    grade: 'UCR 2nd year, Computer Science',
+    role: 'Backend, Model Engineer',
+    hobbies: 'cafe hopping and DJing',
+  },
+  {
+    name: 'Peter Lu',
+    grade: 'UCR 3rd year, Data Science & Business',
+    role: 'Frontend, Support Engineer',
+    hobbies: 'k-pop listening and travel',
+  },
+  {
+    name: 'Xianghao Kong',
+    grade: 'UCR, PhD Computer Science',
+    role: 'General Advisor',
+    hobbies: 'sketching and museum goer',
+  },
+  {
+    name: 'Ratnodeep Bandyopadhyay',
+    grade: 'UCR, PhD Computer Science',
+    role: 'General Advisor',
+    hobbies: 'hiking, camping, tea',
+  },
+];
+
+const TeamSection: React.FC = () => {
+  return (
+    <div>
+      <div className="border-4 flex flex-row flex-wrap justify-center gap-6 w-full">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="shadow-lg w-80">
+            <Card className=" h-full">
+              <CardHeader>
+                <CardTitle className="pb-1 bg-gradient-to-r from-purple-500 to-violet-300 bg-clip-text text-transparent">
+                  {member.name}
+                </CardTitle>
+                <CardDescription>{member.grade}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p className="font-semibold ">{member.role}</p>
+              </CardContent>
+              <CardFooter>
+                <p>Hobbies: {member.hobbies}</p>
+              </CardFooter>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TeamSection;
