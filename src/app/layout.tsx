8a2f5fee05ad0897b513a116acc0469f4c97d8cb
Freddy Song <fredsong99@gmail.com>
2024-08-01 00:13:15 -0700

    chore: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import 'react-toastify/dist/ReactToastify.css';
 import "./globals.css";
 import ClientToastContainer from '@/components/ui/toast-container';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <ClientToastContainer />
+        <Analytics />
       </body>
     </html>
   );
